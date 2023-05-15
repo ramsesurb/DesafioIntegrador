467cf3ce5b518cb3adcd69f313af614937ac238e
ramsesurb <ramsesurb@gmail.com>
2023-05-15 14:21:05 -0300

    ya paginando

--- Routes/cartView.js
+++ Routes/cartView.js
@@ -12,8 +12,16 @@ const cartView = Router();
 cartView.get("/", async (req, res) => {
   const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
   const prodsRaw = await productos.getProducts(limit);
+  //const cart = await cartModel.findById(uid).populate("products.product").lean();
   const prods = prodsRaw.map(item=>item.toObject())
   console.log(prods)
-  res.render("cart", { productos: prods });
+  res.render("carts", { productos: prods });
 });
+cartView.get("/:id", async (req, res) => {
+  const id = parseFloat(req.params.id);
+  const prodById = await productos.getByid(id);
+  console.log(JSON.stringify(prods.producto))
+  res.render("singleCart", { productos: prodById });
+});
+cartView.get
 export default cartView;
